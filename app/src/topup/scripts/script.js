31700b979d00a1dbfef8c97e8d459827a38df96b
Nafis Handoko <naffisioner@gmail.com>
2022-05-29 19:01:20 +0700

    revisi frontend

--- app/src/topup/scripts/script.js
+++ app/src/topup/scripts/script.js
@@ -13,6 +13,32 @@
 
 
 function confirmCheckout(){
-  $('#btn-confirm').css('visibility', 'hidden')
-  $('#virtual-acc-num').val('3333 4444 5555 6666')
+  // $('#btn-confirm').css('visibility', 'hidden')
+  // $('#virtual-acc-num').val('3333 4444 5555 6666')
+  var dict = {
+    payment_id : $('#agen-bank').val(), 
+    gross_amount:$('#amount-num').val(),
+    topup_id: 'topup-' + Math.floor(Date.now() * Math.random())
+  };
+
+  $.ajax({
+      type: "POST", 
+      url: "https://hitech-payment.nafishandoko.repl.co/payment/topup",
+      data : JSON.stringify(dict),
+      contentType: "application/json",
+      crossDomain:true,
+      success: function (resp) {
+        // alert("Access Token :\n"+data.access_token)
+        console.log(resp)
+        // alert(resp.message+"\nPayment Code/VA Number: "+(resp.data.payment_code||resp.data.va_numbers[0].va_number)+"\nJumlah Tagihan: "+resp.data.gross_amount)
+        alert(resp.message)
+        $('#payment-code-num').val(resp.data.payment_code||'Not available')
+        $('#virtual-acc-num').val(resp.data.va_numbers===undefined?'':resp.data.va_numbers[0].va_number)
+        $('#instruction-text').html(resp.data.instruction||'Not available')
+      },
+      error: function (resp) {
+        // alert(data.responseJSON.msg)
+        console.log(resp)
+      },
+  });
 }
